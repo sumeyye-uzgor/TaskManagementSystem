708dcc48212a9ab6f1cb8dc9def92885974c5678
Sumeyye Uzgor <summeyyeoz@gmail.com>
2021-06-09 13:42:55 +0300

    fluid card structure is created at homepage

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,10 +1,22 @@
-
+import TaskCard from "../components/TaskCard.component";
+import { Grid, Box } from "@material-ui/core"
 
 function Home() {
     return (
-        <div className="App">
-            deneme
-        </div>
+        <Box m={5}>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Grid container justify="flex-start" spacing={2}>
+                        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
+                            <Grid key={value} item xs={12} md={3} sm={6} xl={2}>
+                                <TaskCard />
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Grid>
+            </Grid >
+        </Box>
+
     );
 }
 
--- src/pages/Login.page.jsx
+++ src/pages/Login.page.jsx
@@ -1,9 +1,15 @@
-
+import { FormControl, InputLabel, Input, FormHelperText } from '@material-ui/core';
 
 function Login() {
     return (
         <div className="App">
-            login
+            <FormControl>
+                <InputLabel htmlFor="my-input">Email address</InputLabel>
+                <Input id="my-input" aria-describedby="my-helper-text" />
+                <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+            </FormControl>
+
+
         </div>
     );
 }
